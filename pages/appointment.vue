f14937cfedcaa58f42bd3a6478d7a3f2a4a8ef10
Faslur Rahman <faslur.rahman@besolution.com>
2021-12-07 13:25:29 +0530

    changes 07-12-2021

--- pages/appointment.vue
+++ pages/appointment.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <NavInner/>
-        <div class="bg-img " :style="{backgroundImage: `url(` + require(`~/assets/images/bg_contact.webp`) + `)`}">
+        <div class="bg-img-appointment" :style="{backgroundImage: `url(` + require(`~/assets/images/bg_contact.webp`) + `)`}">
         </div>
 
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4 my-4">
@@ -39,11 +39,15 @@
         <label for="Appointment" class="sr-only">Appointment Date and Time</label>
            <div class="Appointment grid grid-cols-1 md:grid-cols-2 gap-4">
              <div class="rounded-md shadow-sm">
-               <el-date-picker lang="en" v-model="datePicker"
+               
+               <fg-input>
+                  <el-date-picker v-model="datePicker"
+                    class="form-input block w-full py-2 px-4 placeholder-gray-500 transition ease-in-out duration-150"
                     type="date"
-                    placeholder="Pick a day" 
-                    class="form-input block w-full py-2 px-4 placeholder-gray-500 transition ease-in-out duration-150">
-                </el-date-picker>
+                    placeholder="Pick a day"
+                    :picker-options="pickerOptions1">
+                 </el-date-picker>
+               </fg-input>
              </div>
              <div class="rounded-md shadow-sm">
                <el-time-select
@@ -93,6 +97,11 @@
       name: "",
       email: "",
       phone: "",
+      pickerOptions1: {
+                    disabledDate(time) {
+                        return time.getTime() < Date.now() - 8.64e7;
+                    }
+      },
       datePicker: '',
       timePicker: ''
     };
@@ -100,6 +109,8 @@
   methods: {
     sendMessage() {
       this.loading = true;
+      
+      if (this.datePicker != '' && this.datePicker != ''){
       this.$axios
         .post("http://localhost:1337/appointments", {
           name: this.name,
@@ -117,18 +128,75 @@
         .finally(() => {
           this.loading = false
         });
+    }
+    else{
+       this.$axios
+        .post("", {
+        }).then(response => {
+          this.errored ='Select a date and time'
+        })
+        .finally(() => {
+          this.loading = false
+        });
+    }
     },
   }
 };
     
 </script>
 
-<style lang="css" scoped>
-.bg-img {
+<style lang="css">
+.bg-img-appointment {
         background-position: center center;
         background-repeat:  no-repeat;
         background-attachment: fixed;
         background-size:  cover;
         height: 30vh;
  }
+.el-picker-panel__body-wrapper, .el-picker-panel.time-select.el-popper{
+   background-color: #fff;
+   padding: 20px;
+   border: 1px solid;
+  padding: 10px;
+  box-shadow: 5px 10px #888888;
+}
+.el-date-picker__header{
+  text-align: center;
+  font-size: 20px;
+}
+.el-date-picker__header:hover {
+  background-color: #D8D8D8;
+}
+body > div.el-picker-panel.el-date-picker.el-popper > div.el-picker-panel__body-wrapper > div > div.el-picker-panel__content > table.el-date-table > tbody > tr:nth-child(1){
+  color:#a3a3a3;
+  text-align: center;
+}
+body > div.el-picker-panel.el-date-picker.el-popper > div.el-picker-panel__body-wrapper > div > div.el-picker-panel__content > table.el-date-table > tbody > tr:nth-child(1) > th, td{
+  padding: 5px !important;
+  text-align: center;
+}
+td:hover{
+ background-color: #000;
+ color: #fff;
+ cursor: pointer;
+}
+td.disabled:hover{
+ background-color: red;
+ color: #fff;
+ cursor: not-allowed;
+}
+.el-scrollbar {
+  padding-bottom:20px;
+  padding-right: 20px;
+}
+.time-select-item{
+  padding-bottom: 5px;
+  padding-left:5px;
+}
+.time-select-item:hover{
+  color: #fff;
+  background-color: #000;
+  cursor: pointer;
+}
+
 </style>
